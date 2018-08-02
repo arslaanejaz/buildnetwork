b81795a0d417a48045458223c6d2e4507ec1e6b5
arslaanejaz <arslaanejaz@gmail.com>
2018-08-02 12:27:47 +0500

    design changes.

---
+++ app/controllers/UserController.js
@@ -0,0 +1,74 @@
+var User = require('../models/user');
+
+var flashOption = {
+    position:"b",
+    duration:"5000"
+  };
+
+var userController = {};
+
+userController.list = (req, res) => {
+    User.find({}).exec((err, users) => {
+      if (err) {
+        console.log("Error:", err);
+      }
+      else {
+
+        res.render("../views/user/list", {users: users});
+      }
+    });
+  };
+
+  userController.save = (req, res) => {
+
+    if(req.body.first_name=='' && req.body.email==''){
+        res.flash('First Name and Email are required!', 'error',flashOption);
+        res.redirect('/register');
+    }else if(req.body.password.length<6){
+        res.flash('Password should be more than 6 letters.', 'error', flashOption);
+        res.redirect('/register');
+    }else{
+        User.findOne({'local.email':req.body.email},(err, usr)=>{
+            if(err) {
+                res.flash('Some Error Occure!', 'error',flashOption);
+                res.redirect('/register');
+            } else {
+                if(usr){
+                    res.flash(req.body.email+' already taken!', 'warn',flashOption);
+                    res.redirect('/register');
+                }else{
+                  user = new User();
+                  user.local.email = req.body.email;
+                  user.local.password = user.generateHash(req.body.password);
+  
+                  user.local.first_name = req.body.first_name;
+                  user.local.last_name = req.body.last_name;
+                  user.local.gender = req.body.gender;
+                  user.local.address1 = req.body.address1;
+                  user.local.address2 = req.body.address2;
+                  user.local.address = req.body.address;
+                  user.local.city = req.body.city;
+                  user.local.zip = req.body.zip;
+
+                  user.save((err,usr)=>{
+                    if(err){
+                        res.flash('Some Error Occure while Registering user.',flashOption);
+                        res.redirect('/register');
+                    }else{
+                        req.session.user = usr;
+                        res.flash('User Registered successfully.',flashOption);
+                        res.redirect('/get-xbn');
+                        }
+                        
+                    });
+                }
+                
+            }});
+    }
+
+
+
+    
+  };
+
+  module.exports = userController;
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,14 @@ var router = express.Router();
 // var mongoose = require('mongoose');
 var User = require('../models/user');
 var auth = require('../middleware/auth');
+var userController = require('../controllers/UserController')
+
 var flashOption = {
     position:"b",
     duration:"5000"
   };
+
+//   router.get('/list', userController.list);
 
 router.get('/register', (req, res, next) => {
     if(req.session.user) 
@@ -81,54 +85,32 @@ router.post('/profile-update', auth, (req, res, next) => {
    
 });
 
-router.post('/sign-up', (req, res, next) => {
-    if(req.body.first_name=='' && req.body.email==''){
-        res.flash('First Name and Email are required!', 'error',flashOption);
-        res.redirect('/register');
-    }else if(req.body.password.length<6){
-        res.flash('Password should be more than 6 letters.', 'error', flashOption);
-        res.redirect('/register');
-    }else{
-        User.findOne({'local.email':req.body.email},(err, usr)=>{
-            if(err) {
-                res.flash('Some Error Occure!', 'error',flashOption);
-                res.redirect('/register');
-            } else {
-                if(usr){
-                    res.flash(req.body.email+' already taken!', 'warn',flashOption);
-                    res.redirect('/register');
-                }else{
-                    user = new User();
-                    user.local.email = req.body.email;
-                    user.local.password = user.generateHash(req.body.password);
-    
-                    user.local.first_name = req.body.first_name;
-                    user.local.last_name = req.body.last_name;
-                    user.local.gender = req.body.gender;
-                    user.local.address1 = req.body.address1;
-                    user.local.address2 = req.body.address2;
-                    user.local.address = req.body.address;
-                    user.local.city = req.body.city;
-                    user.local.zip = req.body.zip;
-    
-                    user.save((err,usr)=>{
-                        if(err){
-                            res.flash('Some Error Occure while Registering user.',flashOption);
-                            res.redirect('/register');
-                        }else{
-                            req.session.user = usr;
-                            res.flash('User Registered successfully.',flashOption);
-                            res.redirect('/get-xbn');
-                            }
-                            
-                        });
-                }
+router.post('/sign-up', userController.save)
+// router.post('/sign-up', (req, res, next) => {
+//     if(req.body.first_name=='' && req.body.email==''){
+//         res.flash('First Name and Email are required!', 'error',flashOption);
+//         res.redirect('/register');
+//     }else if(req.body.password.length<6){
+//         res.flash('Password should be more than 6 letters.', 'error', flashOption);
+//         res.redirect('/register');
+//     }else{
+//         User.findOne({'local.email':req.body.email},(err, usr)=>{
+//             if(err) {
+//                 res.flash('Some Error Occure!', 'error',flashOption);
+//                 res.redirect('/register');
+//             } else {
+//                 if(usr){
+//                     res.flash(req.body.email+' already taken!', 'warn',flashOption);
+//                     res.redirect('/register');
+//                 }else{
+//                     userController.save
+//                 }
                 
-            }});
-    }
+//             }});
+//     }
 
    
-});
+// });
 
 router.post('/sign-in', (req, res, next) => {
     
@@ -183,5 +165,7 @@ router.get('/logout', function(req, res) {
     })
     
   });
+
+
 
 module.exports = router;    
